b4c2ddeed933fbec73c5d286db4d0839a5188930
蒙启章 <mengqizhang@huluwa.cc>
2021-03-02 10:31:12 +0800

    利用freemarker导出pdf，pdf签名解析

---
+++ src/main/java/com/mqz/pdf/tool/PdfToolApplication.java
@@ -0,0 +1,19 @@
+package com.mqz.pdf.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mqz
+ * @since 2021年01月14日
+ * @description
+ * @abount https://github.com/DemoMeng
+ */
+@SpringBootApplication
+public class PdfToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mqz/pdf/tool/PdfToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqz.pdf.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
